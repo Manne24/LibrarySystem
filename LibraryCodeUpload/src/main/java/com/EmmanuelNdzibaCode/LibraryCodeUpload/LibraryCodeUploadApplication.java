f6641fe903db239bfa0754e794cb8a75a75ddb27
Emmanuel <Emmanuel_Ndziba@hotmail.se>
2021-01-02 12:27:17 +0100

    Init Project Files
    initialize Spring Project

---
+++ LibraryCodeUpload/src/main/java/com/EmmanuelNdzibaCode/LibraryCodeUpload/LibraryCodeUploadApplication.java
@@ -0,0 +1,13 @@
+package com.EmmanuelNdzibaCode.LibraryCodeUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCodeUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCodeUploadApplication.class, args);
+	}
+
+}
---
+++ LibraryCodeUpload/src/test/java/com/EmmanuelNdzibaCode/LibraryCodeUpload/LibraryCodeUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmmanuelNdzibaCode.LibraryCodeUpload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCodeUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
